7e77475e6d196032aca75842fd631a4b18d1cb58
camilaroque <milaroque@icloud.com>
2020-04-10 15:54:52 -0300

    commit inicial

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
 const cipher = {
-  // ...
+    encode: function (desloc, getMsg) {
+        if (typeof getMsg !== "string") {
+            throw new TypeError();
+        }
+
+        let msgBox = "";
+        let resCipher = "";
+        let deslocNumber;
+
+        for (let a = 0; a < getMsg.length; a++) {
+            msgBox = getMsg[a];
+
+            if (msgBox.charCodeAt(0) > 64 && msgBox.charCodeAt(0) < 91) {
+                deslocNumber = (((msgBox.charCodeAt(0) - 65 + desloc) % 26) + 65);
+                resCipher += String.fromCharCode(deslocNumber);
+            } else if (msgBox.charCodeAt(0) > 96 && msgBox.charCodeAt(0) < 123) {
+                deslocNumber = (((msgBox.charCodeAt(0) - 97 + desloc) % 26) + 97);
+                resCipher += String.fromCharCode(deslocNumber);
+            } else {
+                deslocNumber = (msgBox.charCodeAt(0));
+                resCipher += String.fromCharCode(deslocNumber);
+            }
+        }
+        return resCipher;
+
+    },
+    decode: function (desloc, getMsg) {
+       
+        let msgBox = "";
+        let resDecipher = "";
+        let deslocNumber;
+
+        for (let a = 0; a < getMsg.length; a++) {
+            msgBox = getMsg[a];
+            if (msgBox.charCodeAt(0) > 64 && msgBox.charCodeAt(0) < 91) {
+                deslocNumber = (((msgBox.charCodeAt(0) - 90 - desloc) % 26) + 90);
+                resDecipher += String.fromCharCode(deslocNumber);
+            } else if (msgBox.charCodeAt(0) > 96 && msgBox.charCodeAt(0) < 123) {
+                deslocNumber = (((msgBox.charCodeAt(0) - 122 - desloc) % 26) + 122);
+                resDecipher += String.fromCharCode(deslocNumber);
+            } else {
+                deslocNumber = (msgBox.charCodeAt(0));
+                resDecipher += String.fromCharCode(deslocNumber);
+            }
+        }
+        return resDecipher;
+    }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import cipher from './cipher.js';
+document.getElementById("btCipher").disabled = true;
+document.getElementById("btDecipher").disabled = true;
 
-console.log(cipher);
+document.getElementById("inputMsg", "offset").addEventListener("input", enableButtons);
+
+function enableButtons() {
+  if (document.getElementById("inputMsg").value !== "" || document.getElementById("offset").value !== "") {
+    document.getElementById("btCipher").disabled = false;
+    document.getElementById("btDecipher").disabled = false;
+  } else {
+    document.getElementById('btCipher').disabled = true;
+    document.getElementById('btDecipher').disabled = true;
+  }
+}
+
+document.getElementById("btCipher").addEventListener("click", cifrar);
+document.getElementById("btDecipher").addEventListener("click", decifrar);
+document.getElementById("clear").addEventListener("click", limpar);
+
+function cifrar() {
+  let getMsg = document.getElementById("inputMsg").value;
+  let desloc = parseInt(document.getElementById('offset').value);
+  let encodeMsg = cipher.encode(desloc, getMsg);
+
+  document.getElementById("result").innerHTML = `Esta é a sua senha Criptografada: <br><br><strong>${encodeMsg}</strong>`
+}
+
+function decifrar() {
+  let getMsg = document.getElementById("inputMsg").value;
+  let desloc = parseInt(document.getElementById('offset').value);
+  let decodeMsg = cipher.decode(desloc, getMsg);
+
+  document.getElementById("result").innerHTML = `Esta é a sua senha Descriptografada: <br><br><strong>${decodeMsg}</strong>`
+}
+
+function limpar() {
+  if (document.getElementById("inputMsg").value !== "" || document.getElementById("offset").value !== "" || document.getElementById("result").innerHTML.value !== "") {
+    document.getElementById("inputMsg").value = "";
+    document.getElementById("offset").value = "";
+    document.getElementById("result").innerHTML = "";
+    enableButtons();
+  }
+}
